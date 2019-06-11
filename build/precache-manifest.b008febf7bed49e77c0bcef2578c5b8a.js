9cfcc36c86d6b5b670700b5cd10e0ad117b6a276
Hugo Zapata <hugozap@gmail.com>
2019-06-11 02:27:28 -0500

    update meta

---
+++ build/precache-manifest.b008febf7bed49e77c0bcef2578c5b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c3743c0dd9155b6c8cf7ae2e57e937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82ad3b7347b0d726adb1",
+    "url": "/static/css/2.9b17ae97.chunk.css"
+  },
+  {
+    "revision": "4cbb09515e5150819feb",
+    "url": "/static/css/main.730359ed.chunk.css"
+  },
+  {
+    "revision": "82ad3b7347b0d726adb1",
+    "url": "/static/js/2.f090c35a.chunk.js"
+  },
+  {
+    "revision": "4cbb09515e5150819feb",
+    "url": "/static/js/main.f85011f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
